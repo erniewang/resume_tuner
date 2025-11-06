08755c4732b66f0f561c66aa2da8dcdbddc42c44
Ernie Wang <erniewang@u.northwestern.edu>
2025-11-06 16:02:48 -0600

    working on a direct pathway type shit

--- electronApp/pages/ResumeTweaks.tsx
+++ electronApp/pages/ResumeTweaks.tsx
@@ -1,55 +1,56 @@
 import { useState, useEffect } from "react";
 import fs from 'fs';
 import YAML from 'yaml';
+import { SessionData } from "react-router-dom";
 
 const file = fs.readFileSync('../input/tempProj.yaml', 'utf8');
 var yamlRawData = YAML.parse(file);
+console.log(yamlRawData);
 
-function RenderCVSection(props: any) {
-    const [key, value] = Object.entries(props)[0] as [string, any];
-    if (value === undefined) return "";
+interface SectionProps {
+    fieldKey: string | number;
+    value: any;
+    combinedPath?: string | number;
 
-    //takes in pair of objects. evaluates k and uses its value as the key
-    const createProps = (k: string | number, v: any) => ({ [k]: v });
+}
 
-    //[] alone would be treated as never[] by typescript
+function RenderCVSection(props: SectionProps) {
+    const { fieldKey, value, combinedPath } = props;
+
+    //fix quote spaces
+    console.log(combinedPath);
+
     const [descriptions, setDescriptions] = useState<string[]>([]);
-
-    const verifyListStr = (value: object) => {
-        if (Array.isArray(value) && value.length > 1 && typeof value[0] == "string") return true;
-        return false;
-    };
+    const isList = Array.isArray(value) && value.length > 1 && typeof value[0] == "string";
 
     useEffect(() => {
-        if (verifyListStr(value)) {
+        if (isList) {
             setDescriptions(value);
         }
     }, []); 
 
-    useEffect(() => {}, [descriptions]); 
+    useEffect(() => {}, [descriptions]);
 
     return (
         <div className="w-full mb-2 bg-gray-100 rounded border p-2">
-            {/* Renders a name if exist. not some index*/}
-            {key.length > 1 && <h3 className="font-semibold mb-2 text-lg">
-                {key.charAt(0).toUpperCase() + key.slice(1)}
+            {fieldKey.toString().length > 1 && <h3 className="font-semibold mb-2 text-lg">
+                {fieldKey.toString().charAt(0).toUpperCase() + fieldKey.toString().slice(1)}
             </h3>}
 
             {typeof value === "object" ? 
                 <>
                 <div className="w-full">
-                    { /* Renders a name if exist. not some index*/ }
-                    {(verifyListStr(value)) ? 
-                        descriptions.map((desc, index) => <RenderCVSection key={index} {...createProps(index, desc)} />) :
-                        Object.entries(value).map(([entryKey, entryValue]) => <RenderCVSection key={entryKey} {...createProps(entryKey, entryValue)} />)
+                    {isList ? 
+                        <>
+                            {descriptions.map((desc, index) => <RenderCVSection fieldKey={index} combinedPath={combinedPath+"."+fieldKey+"["+index+"]"} key={index} value={desc} />)}
+                            <button className="w-[70px] h-[40px] bg-gray-200 rounded-lg text-xl shadow-md"
+                            onClick={() => setDescriptions(descriptions.concat([""]))}>
+                                Add
+                            </button>
+                        </> :
+                        Object.entries(value).map(([entryKey, entryValue]) => <RenderCVSection fieldKey={entryKey} combinedPath={combinedPath+"."+entryKey} key={entryKey} value={entryValue} />)
                     }
                 </div>
-                {(verifyListStr(value)) && 
-                <button className="w-[70px] h-[40px] bg-gray-200 rounded-lg text-xl shadow-md"
-                onClick={() => setDescriptions(descriptions.concat([""]))}
-                >
-                    Add
-                </button>}
                 </> : 
                 (value.length > 100 ? 
                     <textarea 
@@ -69,14 +70,10 @@ function RenderCVSection(props: any) {
 }
 
 export function ResumeTweaks() {
-
-    const [resumeInfo, setResumeInfo] = useState(yamlRawData.cv);
-    const createProps = (k: string, v: any) => ({ [k]: v });
-    
     return (
         <div className='w-full h-full p-4 overflow-y-auto'>
             {Object.entries(yamlRawData.cv).map(([key, value]) => 
-                <RenderCVSection key={key} {...createProps(key, value)} />
+                <RenderCVSection fieldKey={key} combinedPath={"yamlRawData.cv."+key} key={key} value={value} />
             )}
             <button className="fixed bottom-4 right-4 px-4 py-2 mr-2 bg-blue-500 text-white rounded hover:bg-blue-600 z-50">
                 Save
